a3005e11f2a7093bcb5c6e78d499bf1e691ee724
vivek6201 <gvicky4567888@gmail.com>
2024-11-14 15:25:37 +0530

    initialized order, delivery, and vendor services

--- apps/order-service/src/index.ts
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import {configDotenv} from "dotenv"
-
-configDotenv();
-const app = express()
-const PORT = process.env.PORT;
-
-app.listen(PORT, () => console.log(`server is up at port ${PORT}`))
--- apps/vendor-service/src/index.ts
+++ apps/vendor-service/src/index.ts
@@ -1,8 +1,10 @@
 import express from "express";
+import cors from "cors";
 
 const PORT = process.env.PORT;
 const app = express();
 
+app.use(express.json());
+app.use(cors());
 
-
-app.listen(PORT, () => console.log(`server is up at ${PORT}`))
+app.listen(PORT, () => console.log(`server is up at ${PORT}`));
